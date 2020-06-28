22bb4b95d597db94b3f1e59061d15bb089f44a6a
Matt Kukla <43801193+matt-kukla@users.noreply.github.com>
2020-06-27 20:33:22 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyantenna',
+    version='1.0.1',
+    description='Antenna/RF engineering tools',
+    author='Matt Kukla',
+    author_email='matt.kukla@yandex.ru',
+    url='https://github.com/matt-kukla/pyantenna',
+    packages=['pyantenna'],
+)
+
